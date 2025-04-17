5b5e0e240c8f3d73bd0ca4c2e9353899eb5c3cb0
Conan747 <conan747@users.noreply.github.com>
2025-04-17 16:13:48 -0300

    tried to optimize images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    minimumCacheTTL: 2678400, // 31 days
+    formats: ['image/webp'],
     remotePatterns: [
       {
         protocol: "https",
@@ -17,6 +19,9 @@ const nextConfig: NextConfig = {
         hostname: "drive.google.com",
       },
     ],
+    // Defaults below. Uncomment and customize if needed for further optimization.
+    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
     serverActions: {
